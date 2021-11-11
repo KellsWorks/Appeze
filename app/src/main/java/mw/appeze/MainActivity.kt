b94e935d20f59cdd142555b41dd035a139b3ea6f
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-11-11 18:07:07 +0200

    Set up fragments

--- app/src/main/java/mw/appeze/MainActivity.kt
+++ app/src/main/java/mw/appeze/MainActivity.kt
@@ -1,11 +1,16 @@
 package mw.appeze
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import mw.appeze.auth.AuthActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this@MainActivity, AuthActivity::class.java))
+
     }
 }
--- app/src/main/java/mw/appeze/auth/AuthActivity.kt
+++ app/src/main/java/mw/appeze/auth/AuthActivity.kt
@@ -2,11 +2,17 @@ package mw.appeze.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import mw.appeze.R
+import mw.appeze.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
+
+    lateinit var authBinding: ActivityAuthBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+
+        authBinding = DataBindingUtil.setContentView(this, R.layout.activity_auth)
     }
 }
