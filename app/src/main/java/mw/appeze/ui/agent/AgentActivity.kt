5bd46e8bb578dcbba39e2c2c545354d0d0b3b838
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-11-16 08:25:51 +0200

    Set up agent navigation

--- app/src/main/java/mw/appeze/ui/agent/AgentActivity.kt
+++ app/src/main/java/mw/appeze/ui/agent/AgentActivity.kt
@@ -3,8 +3,6 @@ package mw.appeze.ui.agent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.NavHostController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import mw.appeze.R
